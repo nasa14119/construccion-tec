b9cc35691cb536c39ce0053f92e1ddc3c1d43f5b
nasa14119 <nico.amaya0205@gmail.com>
2026-04-10 20:42:19 -0600

    web: cat cafe finish

--- web/cat_cafe/js/nav.js
+++ web/cat_cafe/js/nav.js
@@ -5,12 +5,14 @@ items.forEach((item) => {
     elemento.scrollIntoView({ block: "start", behavior: "smooth" });
   });
 });
-const sections = ["home", "coffee", "adopt"];
+const sections = ["home", "coffee", "adopt", "location"];
 /** @param {IntersectionObserverEntry[]} element*/
 function handleIntersection(element) {
   if (!element || !element[0]) return;
-  const { target: current } =
-    element.find((item) => item.isIntersecting) ?? element[0];
+  const { target: current } = element.find((item) => item.isIntersecting) ?? {
+    target: null,
+  };
+  if (current === null) return;
   const { id } = current;
   items.forEach((item) => {
     item.dataset.href === "#" + id
@@ -21,8 +23,8 @@ function handleIntersection(element) {
 function initObserver() {
   const isDekstop = window.innerWidth >= 768;
   const intersection = new IntersectionObserver(handleIntersection, {
-    threshold: isDekstop ? 0.8 : 0.5,
-    scrollMargin: "5px 0px 0px 0px",
+    threshold: isDekstop ? 0.8 : 0.45,
+    scrollMargin: isDekstop ? "5px 0px 0px 0px" : "10px 0px 0px 0px",
   });
   for (const key of sections) {
     intersection.observe(document.getElementById(key));
